4309870738d59411ae980348efaa87ff9f5f69b2
Juanets <jdgs.gt@gmail.com>
2017-10-03 17:30:56 -0600

    Adding code that counts the frequency of values in a matrix

---
+++ breast cancer/Counting.m
@@ -0,0 +1,18 @@
+function [A] = Counting( Matrix, Cols, values )
+
+    % Count how many times a value appears on a column. This only works
+    % when the range of values is the same for all the columns.
+    
+    % Matrix: the whole matrix of data
+    % Cols: the columns on which we'll be counting
+    % values: vector of unique values
+    
+    h = zeros(length(Cols));
+    for i = 1:length(Cols)
+        for j = 1:length(values)
+            h(i, j) = sum(Matrix(:, i) == values(j));       
+        end
+    end
+    A = [values h'];
+    
+end
