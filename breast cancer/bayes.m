5c448c7f276ba6bc61891092623fe504950db8b7
Juanets <jdgs.gt@gmail.com>
2017-10-06 23:41:42 -0600

    Finished naive bayes: 98% accurate

--- breast cancer/bayes.m
+++ breast cancer/bayes.m
@@ -6,7 +6,7 @@ prob_benign = Prob(Train, 11, 2);
 
 % delete the first column (ids)
 Train(:, 1) = [];
-
+Test(:, 1) = [];
 % vector of column numbers
 Cols = [1:10];
 
@@ -28,3 +28,35 @@ M_norm = Normalize(M_freq);
 
 % normalized benign tumors
 B_norm = Normalize(B_freq);
+
+% predictions vector
+predict = zeros(length(Test),1);
+
+% calculate predictions
+for i = 1:length(Test)
+    prob_m = zeros(9,1)';
+    prob_b = zeros(9,1)';
+
+    for j = 1:9
+        value = Test(i, j);
+        prob_m(j) = M_norm(value, j);
+        prob_b(j) = B_norm(value, j);        
+    end
+
+    malignant = prod(prob_m) * prob_malign;
+    benign = prod(prob_b) * prob_benign;
+
+    if benign > malignant
+        predict(i) = 2;
+    else
+        predict(i) = 4;
+    end
+
+end
+
+% test accuracy
+
+corrects = sum(Test(:,10) == predict);
+accuracy = (corrects * 100) / length(Test); 
+
+% the model is 98% accurate
